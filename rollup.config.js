b63d222a7214f15d99e3593de8d6ea03c641d7c9
0nepeop1e <engshun.lim@gmail.com>
2023-04-29 11:40:42 +0800

    sourcemaps and fix entrypoint

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 /** @type {import('rollup').RollupOptions} */
 export default {
     input: {
-        index: 'src/index.ts',
+        index: 'src/index.ts'
     },
     output: {
         dir: 'dist',
-        format: 'module'
+        format: 'module',
+        sourcemap: true
     },
     plugins: [
         nodeResolve({
             extensions: ['.js', '.ts']
         }),
         commonjs(),
-        typescript()
+        typescript(),
+        sourcemaps()
     ]
 };
